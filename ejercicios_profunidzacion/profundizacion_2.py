6902781c30caf8bf4e4736f7c3d19bceae504449
joserodriz <109117839+joserodriz@users.noreply.github.com>
2022-08-16 00:42:50 -0300

    Add files via upload

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -36,14 +36,27 @@ en la lista pueden usar el método nativo de list "count"
 # --------------------------------
 # Aquí copiar la función "lista_aleatoria"
 # ya elaborada en el ejercicio anterior
+def lista_aleatoria (inicio, fin, cantidad):
+
+    mi_lista = []
+    cantidad_elementos = 0
+
+    while(cantidad_elementos < cantidad):
+        numero = random.randint(inicio, fin)
+        mi_lista.append(numero)
+        cantidad_elementos += 1
+
+    return mi_lista
 
 
 # --------------------------------
 
 # --------------------------------
 # Aquí dentro definir la función contar
+def contar (lista, numero):
 
-
+    veces_repetido = lista.count(numero)
+    return veces_repetido
 # --------------------------------
 
 if __name__ == '__main__':
@@ -52,18 +65,20 @@ if __name__ == '__main__':
     # Alumno: Utilizar la función "lista_aleatoria"
     # para que genere una lista de 5 números que esten comprendidos
     # entre los números 1 al 6 inclusive
-
-    # lista_numeros = lista_aleatoria(...)
+    inicio = 1
+    fin = 6
+    cantidad = 6
+    lista_numeros = lista_aleatoria(inicio,fin,cantidad)
 
     # Imprimir en pantalla "lista_numeros" que tendrá
     # los valores retornado por la función "lista_aleatoria":
 
-    # print(lista_numeros)
+    print(lista_numeros)
 
     # Luego quiero averiguar cuantas veces se repite el numero 3
     # en la lista aleatoria creada
-    # cantidad_tres = contar(lista_numeros, 3)
+    cantidad_tres = contar(lista_numeros, 3)
 
-    # print(cantidad_tres)
+    print(cantidad_tres)
 
     print("terminamos")
